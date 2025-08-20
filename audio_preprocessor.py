28c13236f9640bd8765305cd17c1398408b4e553
Kartikeya Srivastava <kartikeyasrivastava769@gmail.com>
2025-08-20 23:46:02 +0530

    Add audio conversion function to convert files to WAV format

---
+++ audio_preprocessor.py
@@ -0,0 +1,15 @@
+import ffmpeg;
+import os
+
+# Function to convert audio files to WAV format
+def convert_audio_to_wav(input_file):
+    base= input_file.rsplit('.', 1)[0]
+    try:
+        ffmpeg.input(input_file).output(base+".wav", ar=16000, ac=1, format='wav').run(overwrite_output=True)
+        print(f"Converted {input_file} to {input_file}.wav")
+    except ffmpeg.Error as e:
+        print(f"Error converting file: {e}")
+        return False
+    return True 
+
+convert_audio_to_wav("uploads/test.ogg");
