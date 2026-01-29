a360ac67cb4fbe2a0786bf5b3e7dda9f438950f1
Kartikeya Srivastava <kartikeyasrivastava769@gmail.com>
2026-01-29 21:37:47 +0530

    Remove unused save_embedding method from VoiceRepository interface

--- src/application/ports/voice_repository_port.py
+++ src/application/ports/voice_repository_port.py
@@ -2,12 +2,7 @@ from abc import ABC, abstractmethod
 from core.models import SpeakerEmbedding
 
 class VoiceRepository(ABC):
-    
-    @abstractmethod
-    def save_embedding(speaker_label: str, embedding: list[float]) -> None:
-        """Save the speaker embedding to the repository."""
-        pass
-    
+        
     @abstractmethod
     def get_all_embeddings() -> dict[str, list[float]]:
         """Retrieve all speaker embeddings from the repository."""
